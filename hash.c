e6bab4f1cbb33000c877e4ea86466ef8302e8cb7
Erne <ernestodainesiperrot@gmail.com>
2020-06-17 20:02:55 -0300

    agregue structs y hash_crear

---
+++ hash.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "hash.h"
+#define TAM_INI 7
+
+enum estados_celda {OCUPADO, VACIO, BORRADO};
+
+typedef struct celda{
+    void* valor;
+    char* clave;
+    enum estados_celda estado;
+} celda_t;
+
+
+struct hash{
+    hash_destruir_dato_t destruir_dato;
+    void** datos;
+    size_t tamanio;
+};
+
+hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
+    hash_t* hash = malloc(sizeof(hash_t));
+    if(!hash) return NULL;
+    hash->datos = malloc(sizeof(void*) * TAM_INI);
+    if(!hash->datos){
+        free(hash);
+        return NULL;
+    }
+    hash->tamanio = TAM_INI;
+    return hash;
+}
