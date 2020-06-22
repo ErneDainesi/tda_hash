d5329cf69cfbdd9d1a9fe9b0b541a3c49f7e9c10
Nazarena Dos Santos <nazarena.dossantos@intive.com>
2020-06-22 02:32:50 -0300

    style fixes

--- hash.c
+++ hash.c
@@ -8,16 +8,14 @@
 
 typedef enum estados_celda {VACIO, OCUPADO, BORRADO} estado_celda_t;
 
-// Estructuras auxiliares
-
+/* Definición del struct auxiliar celda */
 typedef struct celda{
     void* valor;
     char* clave;
     estado_celda_t estado;
 } celda_t;
 
-// Estructuras
-
+/* Definición del struct hash */
 struct hash{
     celda_t* tabla;
     size_t tamanio;
@@ -26,16 +24,15 @@ struct hash{
     hash_destruir_dato_t destruir_dato;
 };
 
+/* Definición del struct hash_iter */
 struct hash_iter{
 	size_t actual;
     const hash_t* hash;
 };
 
-// Funciones auxiliares
-
-//Nose si meter esto aca, o si al final le mandamos el README con estos links
-//https://medium.com/swlh/hash-tables-in-c-with-the-djb2-algorithm-21f14ba7ca88
-//https://gist.github.com/MohamedTaha98/ccdf734f13299efb73ff0b12f7ce429f
+/* *****************************************************************
+ *                    FUNCIONES AUXILIARES
+ * *****************************************************************/
 
 size_t hash_func(char *str){
     unsigned long hash = 5381;
@@ -43,6 +40,10 @@ size_t hash_func(char *str){
     while((c = *str++))
         hash = ((hash << 5) + hash) + c;
     return hash;
+}
+
+size_t linear_probing(const hash_t *hash, size_t i){
+    return (i + 1) % hash->tamanio;
 }
 
 bool validar_redimension(hash_t* hash){
@@ -58,20 +59,16 @@ celda_t* celda_crear(size_t tamanio_de_tabla){
 }
 
 bool hash_redimension(hash_t* hash){
-    //creo una nueva tabla del doble del tamanio
     celda_t* nueva_tabla = celda_crear((hash->tamanio)*2);   
     if(!nueva_tabla){
         return false;
     }
-    //me guardo la tabla vieja 
     celda_t* tabla_vieja = hash->tabla;
     size_t tamanio_viejo = hash->tamanio;
     hash->tabla = nueva_tabla;
     hash->tamanio = tamanio_viejo * 2;
     hash->cantidad_ocupados = CANT_INI;
     hash->cantidad_borrados = CANT_INI;
-    //recorro todo el hash viejo y si las celdas estan ocupadas las guardo
-    //en la nueva tabla
     for(int i = 0; i < tamanio_viejo; i++){
         if(tabla_vieja[i].estado == OCUPADO){
             hash_guardar(hash, tabla_vieja[i].clave, tabla_vieja[i].valor);
@@ -83,7 +80,9 @@ bool hash_redimension(hash_t* hash){
 }
 
 
-// Primitivas
+/* *****************************************************************
+ *                    PRIMITIVAS DEL HASH
+ * *****************************************************************/
 
 hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
     hash_t* hash = malloc(sizeof(hash_t));
@@ -118,7 +117,7 @@ bool hash_guardar(hash_t *hash, const char *clave, void *dato){
             free(copia_clave);
             return true;
         }
-        i = (i + 1) % hash->tamanio;
+        i = linear_probing(hash, i);
         if (i == pos){
             break;
         }
@@ -145,7 +144,7 @@ void *hash_borrar(hash_t *hash, const char *clave){
             hash->cantidad_borrados++;
             return dato;
         }
-        i = (i + 1) % hash->tamanio;
+        i = linear_probing(hash, i);
         if (i == pos){
             break;
         }
@@ -165,7 +164,7 @@ void *hash_obtener(const hash_t *hash, const char *clave){
             free(copia_clave);
             return dato;
         }
-        i = (i + 1) % hash->tamanio;
+        i = linear_probing(hash, i);
         if (i == pos){
             break;
         }
@@ -184,7 +183,7 @@ bool hash_pertenece(const hash_t *hash, const char *clave){
             free(copia_clave);
             return true;
         }
-        i = (i+1) % hash->tamanio;
+        i = linear_probing(hash, i);
         if (i == pos){
             break;
         }
@@ -210,6 +209,10 @@ void hash_destruir(hash_t *hash){
     free(hash);
 }
 
+/* *****************************************************************
+ *                    PRIMITIVAS DEL ITERADOR
+ * *****************************************************************/
+
 hash_iter_t *hash_iter_crear(const hash_t *hash){
     hash_iter_t* iter = malloc(sizeof(hash_iter_t));
     if (!iter) return NULL;
@@ -220,7 +223,7 @@ hash_iter_t *hash_iter_crear(const hash_t *hash){
     }
     size_t i = 0;
     while (iter->hash->tabla[i].estado != OCUPADO){
-        i = (i+1) % iter->hash->tamanio;
+        i = linear_probing(iter->hash, i);
     }
     iter->actual = i;
     return iter;
